2e5826247a46fdddebc19b3f548982814cb64278
Víctor Hernández <alu0100697032@ull.edu.es>
2015-10-26 21:42:51 +0000

    Finished

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -1,9 +1,16 @@
 package clases;
 
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		FuncionesPR f = new FuncionesPR();
-		System.out.println(f.funcionCero());
+		Scanner input = new Scanner(System.in);
+		System.out.println("Introduzca x: ");
+		int x = input.nextInt();
+		System.out.println("Introduzca y: ");
+		int y = input.nextInt();
+		System.out.println("Potencia de " + x + "^" + y + " = " + f.funcionPotencia(x, y));
 	}
 }
