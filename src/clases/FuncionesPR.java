e08facd0ff248addb8e4623311d16c6609120856
Víctor Hernández <alu0100697032@ull.edu.es>
2015-10-26 21:24:57 +0000

    Product function implemented

--- src/clases/FuncionesPR.java
+++ src/clases/FuncionesPR.java
@@ -2,39 +2,50 @@ package clases;
 
 import java.util.ArrayList;
 
-public class FuncionesPR extends FuncionesBase{
+public class FuncionesPR extends FuncionesBase {
 
 	/*
 	 * Constructor
 	 */
-	
-	public FuncionesPR(){
+
+	public FuncionesPR() {
 		super();
 	}
-	
+
 	/*
 	 * Funcion suma
 	 */
-	
-	public int funcionSuma(int x, int y){
-		if(y == 0){
+
+	public int funcionSuma(int x, int y) {
+		if (y == 0) {
 			ArrayList<Integer> tupla = new ArrayList<Integer>();
 			tupla.add(x);
 			return funcionIdentidad(0, tupla);
-		}
-		else{
+		} else {
 			ArrayList<Integer> tupla = new ArrayList<Integer>();
 			tupla.add(x);
-			tupla.add(y-1);
-			tupla.add(funcionSuma(x, y-1));
+			tupla.add(y - 1);
+			tupla.add(funcionSuma(x, y - 1));
 			return funcionSucesor(funcionIdentidad(2, tupla));
 		}
 	}
-		
+
 	/*
 	 * Funcion producto
 	 */
-	
+
+	public int funcionProducto(int x, int y) {
+		if (y == 0) {
+			return funcionCero();
+		} else {
+			ArrayList<Integer> tupla = new ArrayList<Integer>();
+			tupla.add(x);
+			tupla.add(y - 1);
+			tupla.add(funcionProducto(x, y - 1));
+			return funcionSuma(funcionIdentidad(0, tupla), funcionIdentidad(2, tupla));
+		}
+	}
+
 	/*
 	 * Funcion potencia
 	 */
